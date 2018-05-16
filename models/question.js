999d1b53526c28688c3e3f73b67b4e59386ca920
unknown <tberry6@gmail.com>
2018-05-16 14:14:31 -0700

    Finished api routes and created database models

---
+++ models/question.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+    var Question = sequelize.define("Question", {
+        question: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    Question.associate = function (models) {
+        Question.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        Question.belongsTo(models.SavedCollege, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        Question.hasMany(models.Answer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Question;
+};
--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
+const db = require("./models");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
-});
+db.sequelize.sync({})
+    .then(function () {
+        app.listen(PORT, function () {
+            console.log(`🌎 ==> Server now on port ${PORT}!`);
+        });
+    });
