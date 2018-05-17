8fe6ed14e3238d98392ac06f05daa14c7dadcd38
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-05-17 12:08:18 -0700

    added ranking tab version 1.0

--- client/src/components/rankingTab/tab.js
+++ client/src/components/rankingTab/tab.js
@@ -3,26 +3,37 @@ import "./tab.css";
 
 const tab = props => (
   <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
+    
     <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
+    
+      <ul className="tabs" data-responsive-accordion-tabs="tabs medium-accordion large-tabs" id="example-tabs">
+        <li className="tabs-title is-active"><a href="#panel1" aria-selected="true">Rank your School
+        </a></li>
+        <li className="tabs-title"><a href="#panel2">Tab 2</a></li>
+        <li className="tabs-title"><a href="#panel3">Tab 3</a></li>
       </ul>
+
+      <div className="tabs-content" data-tabs-content="example-tabs">
+        <div className="tabs-panel is-active" id="panel1">
+          <p><h3>University of San Diego</h3></p>
+          <p></p>
+        </div>
+        <div className="tabs-panel" id="panel2">
+          <p>two</p>
+          
+        </div>
+        <div className="tabs-panel" id="panel3">
+          <p>three</p>
+          <p>Check me out! I'm a super cool Tab panel with text content!</p>
+        </div>
+        <div className="tabs-panel" id="panel4">
+          <p>four</p>  
+        </div>
+      </div>
     </div>
-    <span onClick={() => props.removeFriend(props.id)} className="remove">
-      𝘅
-    </span>
-  </div>
+    </div>
+
+      
 );
 
 export default tab;
