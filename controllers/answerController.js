9e9c511d063bf75b2bab1b54e63c0f9de7b09bb0
Yash Arya <yasharya11@gmail.com>
2018-05-17 14:11:38 -0700

    added some controller files

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -1,1 +1,15 @@
 const db = require("../models");
+
+module.exports = {
+    saveAnswer: function (req, res) {
+        db.Question.Create( 
+            { 
+                answer: req.body.answer,
+                QuestionId: req.body.QuestionId
+                // UserId: req.user
+            } 
+        )
+            .then(data => res.json(data))
+            .catch(err => res.status(422).json(err));
+    }
+}
