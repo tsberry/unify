2732ff99d60e6e82549fc75632b5b650e29b3e5f
unknown <tberry6@gmail.com>
2018-05-17 14:34:06 -0700

    Merged

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
-<<<<<<< HEAD
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-=======
-//import Join from "./pages/Join";
-//import Login from "./pages/Login";
->>>>>>> e88e86955f44038cd675322e88d2fffb95243ff5
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import RankingTab from "./components/RankingTab";
+import RankingTab from "./components/rankingTab";
 
 const App = () => (
   <Router>
@@ -23,7 +18,7 @@ const App = () => (
         <Route exact path="/search" component={Search} />
         <Route exact path="/join" component={Signup} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/tab" component={tab} />
+        <Route exact path="/tab" component={RankingTab} />
       </Wrapper>
       <Footer />
     </div>
