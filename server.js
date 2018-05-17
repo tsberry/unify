b61b4fa27f88acb9c753d8f4fa0fba1656ac7a36
unknown <tberry6@gmail.com>
2018-05-17 09:59:55 -0700

    Added college saving route

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
 const db = require("./models");
+const bodyParser = require("body-parser");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(routes);
 
-db.sequelize.sync({})
+db.sequelize.sync({force: true})
     .then(function () {
         app.listen(PORT, function () {
             console.log(`🌎 ==> Server now on port ${PORT}!`);
