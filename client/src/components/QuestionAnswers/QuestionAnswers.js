6f34fca7ec39e9072bdded2dc1fd71db00a46f36
Yash Arya <yasharya11@gmail.com>
2018-05-23 09:29:35 -0700

    asdf

---
+++ client/src/components/QuestionAnswers/QuestionAnswers.js
@@ -0,0 +1,21 @@
+// import React, { Component } from "react";
+// import "./QuestionAnswers.css";
+// import AuthService from "../AuthService";
+
+
+// class QuestionAnswers extends Component {
+
+//   render(){
+//     return(
+//         <div> {auth.getProfile().type === "prospect" ? <div><Quesitons /><AskQuestoin /></div> : <div><Questions /></div>} </div>
+//                                                             //Questions component will have an Answers component within it. 
+//                                                             //If the user is prospect, they may add a questions in the AskQuestion Component
+//                                                             //If the user is an alum, they may add answers into the Answers component
+//                                                             //This is a comment that 
+//     )
+//   }
+// }
+
+// export default QuestionAnswers;
+
+// // <prospective student stuff> : <alum stuff>
