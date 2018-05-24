c31d61fd9dbc32d84e7371c6ad875be5b1f8dca4
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-05-24 10:25:22 -0700

    resized map and added icons

--- client/src/components/IconBar/IconBar.js
+++ client/src/components/IconBar/IconBar.js
@@ -1,6 +1,12 @@
 import "./IconBar.css";
 import API from "../../utils/API";
 import React, { Component } from "react";
+import bookImg from "./icons/book.png";
+import coffee from "./icons/coffee.png"
+import gym from "./icons/dumbbell-for-training.png"
+import restaurant from "./icons/restaurant.png"
+import transport from "./icons/transport.png"
+
 
 
 class IconBar extends Component {
@@ -9,11 +15,11 @@ class IconBar extends Component {
       return(
          <div className = "large-4 "> 
         
-        <a href="#icons"><img src="./icons/book.png"/></a>
-        <a href="#icons"><img src="icons/food.png"/></a>
-        <a href="#icons"><img src="icons/food.png"/></a>
-        <a href="#icons"><img src="icons/food.png"/></a>
-        <a href="#icons"><img src="icons/food.png"/></a>
+        <div id="#icons"><img src={bookImg} /></div>
+        <div id="#icons"><img src={coffee}/></div>
+        <div id="#icons"><img src={gym}/></div>
+        <div id="#icons"><img src={restaurant}/></div>
+        <div id="#icons"><img src={transport}/></div>
          
           </div>
       )
